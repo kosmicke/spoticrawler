173a956549428f821c42fd076ba08b8a9ff58576
André Rodrigues <andre.cs.rodrigues@gmail.com>
2022-02-23 15:35:28 -0300

    improve

--- src/scripts/playlist-add.js
+++ src/scripts/playlist-add.js
@@ -1,24 +1,26 @@
-const webdriver = require("selenium-webdriver");
-const { timeout, findAwait, delay } = require("../config/constants");
-const playlist = require("../data/playlist");
+const webdriver = require('selenium-webdriver')
+const { timeout, findAwait, delay } = require('../config/constants')
+const playlist = require('../data/playlist')
 
-module.exports = async (driver) => {
-  for (let songLink of playlist.songs) {
-    await driver.get(songLink);
+module.exports = async driver => {
+  for (let songId of playlist.songs) {
+    await driver.get(`https://open.spotify.com/track/${songId}`)
 
     const more_btn = await findAwait(
       webdriver.By.css('[data-testid="more-button"]'),
       driver,
       timeout
-    );
-    await more_btn.click();
+    )
+    await more_btn.click()
 
     const add_btn = await driver.findElement(
       webdriver.By.xpath(
         "//div[@id = 'context-menu']//span[contains(text(), 'Add to playlist')]"
       )
-    );
-    await add_btn.click();
+    )
+    await add_btn.click()
+
+    await delay(1000)
 
     const playlist_btn = await findAwait(
       webdriver.By.xpath(
@@ -26,15 +28,15 @@ module.exports = async (driver) => {
       ),
       driver,
       timeout
-    );
-    await playlist_btn.click();
+    )
+    await playlist_btn.click()
 
     await findAwait(
       webdriver.By.xpath(`//*[contains(text(), 'Added to Playlist')]`),
       driver,
       timeout
-    );
+    )
 
-    await delay(2000);
+    await delay(2000)
   }
-};
+}
